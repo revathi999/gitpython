3b306c31130b6b5e9ededf9e63d15c67dd0afab6
revathi <revathinagumothu293@gmail.com>
2018-08-21 14:48:41 -0700

    for file

---
+++ for.py
@@ -0,0 +1,48 @@
+# for i in range(1,6):
+#  print(" "*(6-i)+"*"*i)
+
+
+# for i in range(1,6):
+#     for j in range(1,i):
+#         print(j,end=" ")
+#     print()
+
+
+
+
+# for i in range(1,5):
+#     for j in range(i):
+#          print(j,end=" ")
+#     print()
+
+
+
+# a='abcde'
+# for i in range(0,5):
+# 	print(a[i] * (i + 1))
+
+# for n in range(0,10):
+# 	print(str(n) * n)
+
+
+
+# for j in range(1,6,1):
+# 	for i in range(j,6,1):
+# 	   print(i,end="")
+# 	print()	
+    
+# for j in range(1,7):
+#     for i in range(1,7-j):
+#  	   print(i,end=" ")
+#     print()	
+    
+# for n in range(1,7):
+# 	print(n**1)
+
+
+# odd=1
+# n = eval(input("Enter a number: "))
+# for odd in range(0, n + 1, 2):
+#        odd = odd + 2 
+#       final_square = n + odd
+#     print(n, "squared is", final_square, ".")
