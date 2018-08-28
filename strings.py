52d1664b3284b8102fea1dc37ccb447002443003
revathi <revathinagumothu293@gmail.com>
2018-08-28 11:42:55 -0700

    strings file

---
+++ strings.py
@@ -0,0 +1,30 @@
+# 1
+# b="pythonbestforbeginners"
+# print(b[1::2])
+
+# 2
+# a="my name is nr"
+# print(a[::-1])
+
+# 3
+# a="python"
+# print(a.replace(a[3],'@'))
+
+# 4
+# a="im in a class in kp"
+# print(a.replace("in","out"))
+
+
+# 5
+# a="im in kukatpally"
+# print(a[2::3])
+
+# a=3
+# b=5
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+
